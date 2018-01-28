a3a1789a274f1f5bf1210da596879f27ff9d2073
Kyler KY <kylerky@live.com>
2018-01-28 23:33:47 +0800

    change the underlying union to variant so that the construction and destruction are handled correctly
    thanks to Tibor Djurica Potpara

--- graph/multiset.hxx
+++ graph/multiset.hxx
@@ -1,19 +1,104 @@
 #ifndef GPSOINN_MULTISET_HXX
 #define GPSOINN_MULTISET_HXX
 
+#include <cstring>
 #include <functional>
 #include <iterator>
 #include <vector>
 
 namespace GPSOINN {
 
-template <typename Value> union multiset_value_type {
-    size_t next;
-    Value value;
+template <size_t... args> struct static_max;
+
+template <size_t arg> struct static_max<arg> {
+    static const size_t value = arg;
 };
+
+template <size_t arg1, size_t arg2, size_t... others>
+struct static_max<arg1, arg2, others...> {
+    static const size_t value = arg1 >= arg2
+                                    ? static_max<arg1, others...>::value
+                                    : static_max<arg2, others...>::value;
+};
+
+template <typename T, typename T_2> struct multiset_variant {
+  private:
+    using data_t = typename std::aligned_storage<
+        static_max<sizeof(T), sizeof(T_2)>::value,
+        static_max<alignof(T), alignof(T_2)>::value>::type;
+    bool bit;
+    data_t data;
+
+    inline void copy(const data_t *other) {
+        if (bit)
+            new (&data) T(*reinterpret_cast<const T *>(other));
+        else
+            new (&data) T_2(*reinterpret_cast<const T_2 *>(other));
+    }
+    inline void move(const data_t *other) {
+        if (bit)
+            new (&data) T(std::move(*reinterpret_cast<const T *>(other)));
+        else
+            new (&data)
+                T_2(std::move(*reinterpret_cast<const T_2 *>(other)));
+    }
+    inline void destroy() {
+        if (bit)
+            reinterpret_cast<T_2 *>(&data)->~T_2();
+        else
+            reinterpret_cast<T *>(&data)->~T();
+    }
+
+  public:
+    multiset_variant() : bit(false) { new (&data) T(); }
+    // copy constructor
+    multiset_variant(const multiset_variant &other) : bit(other.bit) {
+        copy(&other.data);
+    }
+    // move constructor
+    multiset_variant(multiset_variant &&other) : bit(other.bit) {
+        move(&other.data);
+    }
+
+    ~multiset_variant() { destroy(); }
+
+    multiset_variant &operator=(multiset_variant other) {
+        bit = other.bit;
+        copy(&other.data);
+        return *this;
+    }
+
+    bool second() const { return bit; }
+
+    template <typename Type, typename... Args> void set(Args &&... args) {
+        using std::is_same;
+
+        destroy();
+
+        new (&data) Type(std::forward<Args>(args)...);
+        bit = is_same<Type, T_2>::value;
+    }
+
+    template <typename Type> Type &get() {
+        using std::is_same;
+        if (bit == is_same<Type, T_2>::value) {
+            return *reinterpret_cast<Type *>(&data);
+        } else
+            throw std::bad_cast();
+    }
+    template <typename Type> const Type &get() const {
+        using std::is_same;
+        if (bit == is_same<Type, T_2>::value) {
+            return *reinterpret_cast<const Type *>(&data);
+        } else
+            throw std::bad_cast();
+    }
+};
+
 template <typename Key, typename Compare = std::less<Key>,
-          typename Allocator = std::allocator<multiset_value_type<Key>>>
-class multiset : protected std::vector<multiset_value_type<Key>, Allocator> {
+          typename Allocator = std::allocator<multiset_variant<size_t, Key>>>
+class multiset
+    : protected std::vector<multiset_variant<size_t, Key>, Allocator> {
   public:
     class iterator;
     class const_iterator;
@@ -25,7 +110,7 @@ class multiset : protected std::vector<multiset_value_type<Key>, Allocator> {
     typedef Key value_type;
     typedef Compare key_compare;
     typedef Compare value_compare;
-    typedef std::vector<multiset_value_type<value_type>, Allocator> vector;
+    typedef std::vector<multiset_variant<size_t, Key>, Allocator> vector;
 
     typedef typename vector::allocator_type allocator_type;
     typedef typename vector::size_type size_type;
@@ -44,6 +129,13 @@ class multiset : protected std::vector<multiset_value_type<Key>, Allocator> {
     multiset(multiset &&other) noexcept = default;
     multiset(multiset &&other, const Allocator &alloc);
 
+    friend void swap(multiset &first, multiset &second) noexcept {
+        using std::swap;
+        swap(dynamic_cast<vector &>(first), dynamic_cast<vector &>(second));
+        swap(first.less, second.less);
+        swap(first.count, second.count);
+        swap(first.first_free, second.first_free);
+    }
     /* destructors */
     virtual ~multiset() = default;
 
@@ -58,7 +150,6 @@ class multiset : protected std::vector<multiset_value_type<Key>, Allocator> {
     // clear
     void clear() noexcept {
         vector::clear();
-        valid_bits.clear();
         first_free = 0;
         count = 0;
     };
@@ -68,8 +159,7 @@ class multiset : protected std::vector<multiset_value_type<Key>, Allocator> {
     size_type size() const noexcept { return count; };
 
     /* operators */
-    multiset &operator=(const multiset &other);
-    multiset &operator=(multiset &&other) noexcept;
+    multiset &operator=(multiset other);
     value_type &operator[](size_type index);
     const value_type &operator[](size_type index) const;
 
@@ -88,11 +178,9 @@ class multiset : protected std::vector<multiset_value_type<Key>, Allocator> {
     const_iterator get_iterator(size_type index) const noexcept;
 
   private:
-    typedef std::vector<bool> bit_vector;
     Compare less;
     size_type count = 0;
     size_type first_free = 0;
-    bit_vector valid_bits;
 };
 
 } // namespace GPSOINN
@@ -119,16 +207,15 @@ class multiset<Key, Compare, Allocator>::iterator {
     typedef std::bidirectional_iterator_tag iterator_category;
 
     iterator() : vector(nullptr) {}
+    virtual ~iterator() = default;
 
     // dereference
-    reference operator*() const { return iter->value; }
-    pointer operator->() const { return &(iter->value); }
+    reference operator*() const { return iter->template get<Key>(); }
+    pointer operator->() const { return &(operator*()); }
     // increment
     iterator &operator++() {
-        ++bit_iter;
         ++iter;
-        while (iter != vector->end() && !*bit_iter) {
-            ++bit_iter;
+        while (iter != vector->end() && !iter->second()) {
             ++iter;
         }
         return *this;
@@ -140,8 +227,7 @@ class multiset<Key, Compare, Allocator>::iterator {
     }
     // decrement
     iterator &operator--() {
-        while (iter != vector->begin() && !*bit_iter) {
-            --bit_iter;
+        while (iter != vector->begin() && !iter->second()) {
             --iter;
         }
         return *this;
@@ -155,7 +241,6 @@ class multiset<Key, Compare, Allocator>::iterator {
   private:
     typename vector::iterator iter;
     const vector *vector;
-    typename bit_vector::const_iterator bit_iter;
 };
 
 template <typename Key, typename Compare, typename Allocator>
@@ -179,17 +264,17 @@ class multiset<Key, Compare, Allocator>::const_iterator {
 
     const_iterator() : vector(nullptr) {}
     const_iterator(const iterator &other)
-        : iter(other.iter), vector(other.vector), bit_iter(other.bit_iter) {}
+        : iter(other.iter), vector(other.vector) {}
+
+    virtual ~const_iterator() = default;
 
     // dereference
-    reference operator*() const { return iter->value; }
-    pointer operator->() const { return &(iter->value); }
+    reference operator*() const { return iter->template get<Key>(); }
+    pointer operator->() const { return &(operator*()); }
     // increment
     const_iterator &operator++() {
-        ++bit_iter;
         ++iter;
-        while (iter != vector->cend() && !*bit_iter) {
-            ++bit_iter;
+        while (iter != vector->cend() && !iter->second()) {
             ++iter;
         }
         return *this;
@@ -201,8 +286,7 @@ class multiset<Key, Compare, Allocator>::const_iterator {
     }
     // decrement
     const_iterator &operator--() {
-        while (iter != vector->cbegin() && !*bit_iter) {
-            --bit_iter;
+        while (iter != vector->cbegin() && !iter->second()) {
             --iter;
         }
         return *this;
@@ -216,7 +300,6 @@ class multiset<Key, Compare, Allocator>::const_iterator {
   private:
     typename vector::const_iterator iter;
     const vector *vector;
-    typename bit_vector::const_iterator bit_iter;
 };
 
 template <typename T, typename Compare, typename Allocator>
@@ -224,24 +307,15 @@ typename multiset<T, Compare, Allocator>::iterator
 multiset<T, Compare, Allocator>::insert(const T &value) {
     if (count == vector::size()) {
         // construct the union to be inserted
-        multiset_value_type<T> elem;
-        elem.value = value;
-        // maintain the valid bits
-        valid_bits.push_back(1);
+        multiset_variant<size_t, T> elem;
+        elem.template set<T>(value);
         // call the push back
-        typename vector::iterator iter;
-        try {
-            vector::push_back(elem);
-        } catch (...) {
-            valid_bits.pop_back();
-            throw;
-        }
+        vector::push_back(elem);
         // increment count
         ++count;
 
         iterator result;
         result.iter = vector::begin() + (vector::size() - 1);
-        result.bit_iter = valid_bits.begin() + (vector::size() - 1);
         result.vector = this;
         return result;
     }
@@ -251,18 +325,16 @@ multiset<T, Compare, Allocator>::insert(const T &value) {
     // store the position
     size_type pos = first_free;
 
-    valid_bits[pos] = 1;
     // pop front operation on the free list
-    first_free = elem.next;
+    first_free = elem.template get<size_t>();
     // assign
-    elem.value = value;
+    elem.template set<T>(value);
 
     // increment count
     ++count;
 
     iterator result;
     result.iter = vector::begin() + pos;
-    result.bit_iter = valid_bits.begin() + pos;
     result.vector = this;
     return result;
 }
@@ -272,24 +344,15 @@ typename multiset<T, Compare, Allocator>::iterator
 multiset<T, Compare, Allocator>::insert(T &&value) {
     if (count == vector::size()) {
         // construct the union to be inserted
-        multiset_value_type<T> elem;
-        elem.value = value;
-        // maintain the valid bits
-        valid_bits.push_back(1);
+        multiset_variant<size_t, T> elem;
+        elem.template set<T>(value);
         // call the push back
-        typename vector::iterator iter;
-        try {
-            vector::push_back(std::move(elem));
-        } catch (...) {
-            valid_bits.pop_back();
-            throw;
-        }
+        vector::push_back(std::move(elem));
         // increment count
         ++count;
 
         iterator result;
         result.iter = vector::begin() + (vector::size() - 1);
-        result.bit_iter = valid_bits.begin() + (vector::size() - 1);
         result.vector = this;
         return result;
     }
@@ -299,18 +362,16 @@ multiset<T, Compare, Allocator>::insert(T &&value) {
     // store the position
     size_type pos = first_free;
 
-    valid_bits[pos] = 1;
     // pop front operation on the free list
-    first_free = elem.next;
+    first_free = elem.template get<size_t>();
     // assign
-    elem.value = value;
+    elem.template set<T>(value);
 
     // increment count
     ++count;
 
     iterator result;
     result.iter = vector::begin() + pos;
-    result.bit_iter = valid_bits.begin() + pos;
     result.vector = this;
     return result;
 }
@@ -323,13 +384,10 @@ multiset<Key, Compare, Allocator>::erase(const_iterator pos) {
 
     iterator result;
     result.iter = iter;
-    result.bit_iter = pos.bit_iter;
     result.vector = pos.vector;
     ++result;
 
-    valid_bits[index] = 0;
-
-    iter->next = first_free;
+    iter->template set<size_t>(first_free);
     first_free = index;
 
     // decrement count
@@ -347,8 +405,8 @@ multiset<Key, Compare, Allocator>::erase(const_iterator beg,
 
     iterator result;
     result.iter = vector::begin + (end.iter - vector::cbegin());
-    result.bit_iter = end.bit_iter;
-    result.vector = end.bit_iter;
+    result.vector = this;
+
     return result;
 }
 
@@ -372,10 +430,8 @@ typename multiset<Key, Compare, Allocator>::iterator
 multiset<Key, Compare, Allocator>::begin() noexcept {
     iterator iter;
     iter.iter = vector::begin();
-    iter.bit_iter = valid_bits.cbegin();
-    while (iter.iter != vector::end() && !*iter.bit_iter) {
+    while (iter.iter != vector::end() && !iter.iter->second()) {
         ++iter.iter;
-        ++iter.bit_iter;
     }
     iter.vector = this;
     return iter;
@@ -386,10 +442,8 @@ typename multiset<Key, Compare, Allocator>::const_iterator
 multiset<Key, Compare, Allocator>::begin() const noexcept {
     iterator iter;
     iter.iter = vector::cbegin();
-    iter.bit_iter = valid_bits.cbegin();
-    while (iter.iter != vector::cend() && !*iter.bit_iter) {
+    while (iter.iter != vector::cend() && !iter->second()) {
         ++iter.iter;
-        ++iter.bit_iter;
     }
     iter.vector = this;
     return iter;
@@ -400,10 +454,8 @@ typename multiset<Key, Compare, Allocator>::const_iterator
 multiset<Key, Compare, Allocator>::cbegin() const noexcept {
     const_iterator iter;
     iter.iter = vector::cbegin();
-    iter.bit_iter = valid_bits.cbegin();
-    while (iter.iter != vector::cend() && !*iter.bit_iter) {
+    while (iter.iter != vector::cend() && !iter.iter->second()) {
         ++iter.iter;
-        ++iter.bit_iter;
     }
     iter.vector = this;
     return iter;
@@ -414,7 +466,6 @@ typename multiset<Key, Compare, Allocator>::iterator
 multiset<Key, Compare, Allocator>::end() noexcept {
     iterator iter;
     iter.iter = vector::end();
-    iter.bit_iter = valid_bits.cend();
     iter.vector = this;
     return iter;
 }
@@ -424,7 +475,6 @@ typename multiset<Key, Compare, Allocator>::const_iterator
 multiset<Key, Compare, Allocator>::end() const noexcept {
     iterator iter;
     iter.iter = vector::cend();
-    iter.bit_iter = valid_bits.cend();
     iter.vector = this;
     return iter;
 }
@@ -434,7 +484,6 @@ typename multiset<Key, Compare, Allocator>::const_iterator
 multiset<Key, Compare, Allocator>::cend() const noexcept {
     const_iterator iter;
     iter.iter = vector::cend();
-    iter.bit_iter = valid_bits.cend();
     iter.vector = this;
     return iter;
 }
@@ -442,33 +491,19 @@ multiset<Key, Compare, Allocator>::cend() const noexcept {
 template <typename Key, typename Compare, typename Allocator>
 multiset<Key, Compare, Allocator>::multiset(const multiset &other,
                                             const Allocator &alloc)
-    : vector(other, alloc), valid_bits(other.valid_bits), less(other.less),
-      count(other.count), first_free(other.first_free) {}
+    : vector(other, alloc), less(other.less), count(other.count),
+      first_free(other.first_free) {}
 
 template <typename Key, typename Compare, typename Allocator>
 multiset<Key, Compare, Allocator>::multiset(multiset &&other,
                                             const Allocator &alloc)
-    : vector(other, alloc), valid_bits(other.valid_bits), less(other.less),
-      count(other.count), first_free(other.first_free) {}
+    : vector(other, alloc), less(other.less), count(other.count),
+      first_free(other.first_free) {}
 
 template <typename Key, typename Compare, typename Allocator>
 multiset<Key, Compare, Allocator> &multiset<Key, Compare, Allocator>::
-operator=(const multiset &other) {
-    vector::operator=(other);
-    valid_bits = other.valid_bits;
-    less = other.less;
-    count = other.count;
-    first_free = other.first_free;
-    return *this;
-}
-template <typename Key, typename Compare, typename Allocator>
-multiset<Key, Compare, Allocator> &multiset<Key, Compare, Allocator>::
-operator=(multiset &&other) noexcept {
-    vector::operator=(other);
-    valid_bits = other.valid_bits;
-    less = other.less;
-    count = other.count;
-    first_free = other.first_free;
+operator=(multiset other) {
+    std::swap(*this, other);
     return *this;
 }
 template <typename Key, typename Compare, typename Allocator>
@@ -482,7 +517,6 @@ typename multiset<Key, Compare, Allocator>::iterator
 multiset<Key, Compare, Allocator>::get_iterator(size_type index) noexcept {
     iterator iter;
     iter.iter = vector::begin() + index;
-    iter.bit_iter = valid_bits.cbegin() + index;
     iter.vector = this;
     return iter;
 }
@@ -492,7 +526,6 @@ multiset<Key, Compare, Allocator>::get_iterator(size_type index) const
     noexcept {
     const_iterator iter;
     iter.iter = vector::cbegin() + index;
-    iter.bit_iter = valid_bits.cbegin() + index;
     iter.vector = this;
     return iter;
 }
